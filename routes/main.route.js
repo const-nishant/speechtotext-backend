42a0636848983e680164297a0853e0bfe651fec4
const-nishant <nishantspatil04@gmail.com>
2025-06-05 13:22:16 +0530

    fix: update multer storage configuration for file uploads

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,8 +2,17 @@ import express from "express";
 import { uploadFile } from "../controllers/upload.controller.js";
 import { exportFile } from "../controllers/export.controller.js";
 import multer from "multer";
+import path from "path";
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${Date.now()}${ext}`);
+  },
+});
+const upload = multer({ storage });
+
 const mainRouter = express.Router();
 
 mainRouter.post("/upload", upload.single("audio"), uploadFile);
