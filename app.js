deb7256eead45d9d454d8fa7fbd63dbe288e4735
const-nishant <nishantspatil04@gmail.com>
2025-06-06 11:16:11 +0530

    feat: add CORS support for improved API access

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import transcribeAudio from "./transcribe.js";
 import fs from "fs";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 dotenv.config();
 const app = express();
 
@@ -13,27 +14,36 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/api/v1/", mainRouter);
-
 app.get("/", (req, res) => {
   res.send("Welcome to speech-to-text-backend!");
 });
 
 io.on("connection", (socket) => {
   console.log("Client connected");
+  let audioChunks = [];
 
-  socket.on("audio-stream", async (data) => {
+  socket.on("audio-stream", (data) => {
     const buffer = Buffer.from(data.audioChunk, "base64");
-    fs.writeFileSync("temp_audio.wav", buffer);
+    audioChunks.push(buffer);
+  });
+
+  socket.on("stop-audio-stream", async (data) => {
+    const audioBuffer = Buffer.concat(audioChunks);
+    fs.writeFileSync("temp_audio.wav", audioBuffer);
     const transcript = await transcribeAudio(
       "temp_audio.wav",
       data.language || "en"
     );
     socket.emit("transcript-result", transcript);
-  }),
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-    });
+    audioChunks = []; // reset for next recording
+    fs.unlinkSync("temp_audio.wav"); // Clean up the temporary file
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 server.listen(process.env.PORT || 3000, () => {
